d3ff33ee355864d0bd15c9580618506a6c64cd86
Hienz <francispanogao@gmail.com>
2026-01-11 15:46:39 +0800

    add a pending book class file

---
+++ ADMIN/Pending_Books.java
@@ -0,0 +1,26 @@
+package ADMIN;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class Pending_Books extends javax.swing.JPanel{
+    private MainFrame frame;
+    public Pending_Books(MainFrame frame){
+        this.frame = frame;
+        panel();
+    }
+    public void panel(){
+        this.setLayout(null);
+        this.setPreferredSize(new Dimension(1512, 982));
+
+        JLabel background = new JLabel();
+        background.setBounds(0,0,1512,982);
+        background.setIcon(new ImageIcon("PEKTURS\\Book Management (pending)1.png"));
+        background.setLayout(null);
+    }
+}
