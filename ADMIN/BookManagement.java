1a5a98175284f07ade26ed8b509ada1ed6b6651d
Hienz <francispanogao@gmail.com>
2026-01-11 15:44:20 +0800

    add a book management class file

--- ADMIN/BookManagement.java
+++ ADMIN/BookManagement.java
@@ -20,18 +20,12 @@ public class BookManagement extends javax.swing.JPanel {
 
         JLabel background = new JLabel();
         background.setBounds(0, 0, 1512, 982);
-        background.setIcon(new ImageIcon("PEKTURS/Book Management.png"));
+        background.setIcon(new ImageIcon("PEKTURS/Book Management (ADMIN)1-4.png"));
         background.setLayout(null);
 
        background.setLayout(null);
 
-    JLabel text = new JLabel("Book management 'to, 'wag magulo");
-    text.setBounds(400, 491, 1000, 100);
-    text.setFont(new Font("Arial", Font.BOLD, 50));
-    text.setForeground(Color.BLACK);
-
-
-
+   
     JButton Back_to_dashboard = new JButton();
         Back_to_dashboard.setBounds(40, 284, 343, 68);
         Back_to_dashboard.setOpaque(false);
@@ -48,11 +42,58 @@ public class BookManagement extends javax.swing.JPanel {
             }
         });
 
+        JButton availableBook = new JButton();
+        availableBook.setBounds(145, 437, 170, 24);
+        availableBook.setOpaque(false);
+        availableBook.setContentAreaFilled(false);
+        availableBook.setBorderPainted(false);
+        availableBook.setFocusPainted(false);
 
+        availableBook.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("switching to Avaialble Books");
+                frame.setContentPane(new Available_Books(frame));
+                frame.revalidate();
+                frame.pack();
+            }
+        });
 
+        JButton borrowed = new JButton();
+        borrowed.setBounds(132, 475, 170, 24);
+        borrowed.setOpaque(false);
+        borrowed.setContentAreaFilled(false);
+        borrowed.setBorderPainted(false);
+        borrowed.setFocusPainted(false);
 
+        borrowed.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("switching to borrowed Books");
+                frame.setContentPane(new Borrowed_Books(frame));
+                frame.revalidate();
+                frame.pack();
+            }
+        });
+
+        JButton pending = new JButton();
+        pending.setBounds(145, 437, 170, 24);
+        pending.setOpaque(false);
+        pending.setContentAreaFilled(false);
+        pending.setBorderPainted(false);
+        pending.setFocusPainted(false);
+
+        pending.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.out.println("switching to pending Books");
+                frame.setContentPane(new Pending_Books(frame));
+                frame.revalidate();
+                frame.pack();
+            }
+        });
+        
+        background.add(availableBook);
         background.add(Back_to_dashboard);
-        background.add(text); 
+        background.add(borrowed);
+        background.add(pending);
         frame.add(background);
         this.add(background);
 
